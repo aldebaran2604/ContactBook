08739ee2c8408458721b3f899d563f7ccce021ed
aldebaran2604 <luis-jose-26041987@hotmail.com>
2022-07-26 23:09:10 -0500

    Add new columns to model Contact Issue #16

---
+++ ContactPersistence/Migrations/20220727040727_AddNewColumnsToContact.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactPersistence.Migrations
+{
+    public partial class AddNewColumnsToContact : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Birthday",
+                table: "Contacts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Contacts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Contacts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Contacts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                table: "Contacts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Contacts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "Contacts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Relationship",
+                table: "Contacts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "Contacts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StreetDirection1",
+                table: "Contacts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StreetDirection2",
+                table: "Contacts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TypeRelationship",
+                table: "Contacts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WebSite",
+                table: "Contacts",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthday",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "Relationship",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "StreetDirection1",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "StreetDirection2",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "TypeRelationship",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "WebSite",
+                table: "Contacts");
+        }
+    }
+}
--- ContactPersistence/Models/Contact.cs
+++ ContactPersistence/Models/Contact.cs
@@ -4,12 +4,46 @@ namespace ContactPersistence.Models;
 public class Contact
 {
     public int ContactId { get; set; }
+
     public string? Names { get; set; }
+
     public string? LastNames { get; set; }
+
     public string? Pseudonymous { get; set; }
+
     public string? CompanyName { get; set; }
+
     public int? BusinessPositionID { get; set; }
+
     public BusinessPosition? BusinessPosition { get; set; }
+
     public int? BusinessDepartmentId { get; set; }
+
     public BusinessDepartment? BusinessDepartment { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public string? Country { get; set; }
+
+    public string? StreetDirection1 { get; set; }
+
+    public string? StreetDirection2 { get; set; }
+
+    public string? PostalCode { get; set; }
+
+    public string? City { get; set; }
+
+    public string? State { get; set; }
+
+    public DateTime? Birthday { get; set; }
+
+    public string? WebSite { get; set; }
+
+    public string? Relationship { get; set; }
+
+    public string? TypeRelationship { get; set; }
+
+    public string? Notes { get; set; }
 }

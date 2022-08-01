aa8015b5c51653a188e83b620e334869e4e5db85
aldebaran2604 <luis-jose-26041987@hotmail.com>
2022-08-01 08:56:17 -0500

    Add comments in test project Issue #20

--- ContactMSTest/PersistenceTests/ContactTest.cs
+++ ContactMSTest/PersistenceTests/ContactTest.cs
@@ -23,16 +23,17 @@ public class ContactTest
     [ClassCleanup()]
     public static void ClassCleanup()
     {
-        
+
     }
 
     [TestInitialize]
     public void MethodInit()
     {
+        //Generate new random data person for each method
         Person person = RandomData.GeneratePerson<Person>();
         contact.Names = person.FirstName;
         contact.LastNames = person.LastName;
-        contact. Pseudonymous = RandomData.GenerateWord(50);
+        contact.Pseudonymous = RandomData.GenerateWord(50);
         contact.Email = person.Email;
         contact.PhoneNumber = person.CellPhone;
         contact.Country = person.Country;
@@ -56,7 +57,7 @@ public class ContactTest
     [TestMethod]
     public void AddContactTest()
     {
-        //Save 
+        //Save new contact
         IResponseInformation responseInformation = BLContact.AddContact(contact);
 
         //Validate the response information
@@ -66,9 +67,11 @@ public class ContactTest
     [TestMethod]
     public void AddContactTestWithLastBusinessDepartmentOrPosition()
     {
+        //Get list of Business Department
         IResponseInformation<BusinessDepartment[]> listBusinessDepartment = BLBusinessDepartment.ListBusinessDepartment();
-        BusinessDepartment? businessDepartment =  listBusinessDepartment.ResultItem?.LastOrDefault();
-        
+        BusinessDepartment? businessDepartment = listBusinessDepartment.ResultItem?.LastOrDefault();
+
+        //Get list of Business Position
         IResponseInformation<BusinessPosition[]> listBusinessPosition = BLBusinessPosition.ListBusinessPosition();
         BusinessPosition? businessPosition = listBusinessPosition.ResultItem?.LastOrDefault();
 
@@ -77,7 +80,8 @@ public class ContactTest
 
         //Add Business Position id to contact
         contact.BusinessPositionID = businessPosition?.BusinessPositionId;
-        
+
+        //Saved the new contact
         IResponseInformation responseInformation = BLContact.AddContact(contact);
 
         //Validate the response information
@@ -87,37 +91,45 @@ public class ContactTest
     [TestMethod]
     public void AddContactTestWithNewBusinessDepartmentAndPosition()
     {
+        //Create the new department with random data
         BusinessDepartment businessDepartment = new BusinessDepartment()
         {
             Name = RandomData.GenerateWord(120),
             Description = RandomData.GenerateWord(250)
         };
 
+        //Create the new position with random data
         BusinessPosition businessPosition = new BusinessPosition()
         {
             Name = RandomData.GenerateWord(120),
             Description = RandomData.GenerateWord(250)
         };
-        
+
         //Add new Business Department and related to contact
         contact.BusinessDepartment = businessDepartment;
 
         //Add new Business Position and related to contact
         contact.BusinessPosition = businessPosition;
-        
+
+        //Saved the new contact
         IResponseInformation responseInformation = BLContact.AddContact(contact);
 
         //Validate the response information
         Assert.IsTrue(responseInformation.Success, responseInformation.Message);
     }
-    
+
     [TestMethod]
     public void DeleteContactTest()
     {
+        //Get list of contact
         IResponseInformation<Contact[]> responseInformation = BLContact.ListContacts();
+
+        //Searched the first contact to deleted
         Contact contactDelete = responseInformation.ResultItem?.FirstOrDefault() ?? new Contact();
+
+        //Delete the contact
         IResponseInformation responseInformationDelete = BLContact.DeleteContact(contactDelete);
-        
+
         //Validate the response information
         Assert.IsTrue(responseInformationDelete.Success, responseInformationDelete.Message);
     }

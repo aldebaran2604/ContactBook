b5d846401c6637f2bd03ee775b7fc638dc0bce88
aldebaran2604 <luis-jose-26041987@hotmail.com>
2022-08-25 17:37:44 -0500

    Create grpc services and implement the tests in the mstest project Issue #23

---
+++ ContactMSTest/ServiceGRPCTest/BusinessDepartmentTest.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Linq;
+using ContactServiceGRPC;
+using dotNetTips.Utility.Standard.Tester;
+using Grpc.Net.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ContactMSTest.ServiceGRPCTest;
+
+[TestClass]
+public class BusinessDepartmentTest
+{
+    [TestMethod]
+    public void ListBusinessDepartmentTest()
+    {
+        // The port number must match the port of the gRPC server.
+        using var channel = GrpcChannel.ForAddress("https://localhost:7108");
+        var businessDepartmentServiceClient = new BusinessDepartmentService.BusinessDepartmentServiceClient(channel);
+        var listBusinessDepartmentResponse = businessDepartmentServiceClient.ListBusinessDepartment(new ListBusinessDepartmentRequest());
+
+        Assert.IsTrue(listBusinessDepartmentResponse.Success, listBusinessDepartmentResponse.Message);
+    }
+
+    [TestMethod]
+    public void AddBusinessDepartmentTest()
+    {
+        // The port number must match the port of the gRPC server.
+        using var channel = GrpcChannel.ForAddress("https://localhost:7108");
+        var businessDepartmentServiceClient = new BusinessDepartmentService.BusinessDepartmentServiceClient(channel);
+        var businessDepartmentModel = new BusinessDepartmentModel()
+        {
+            Name = "GRPC" + RandomData.GenerateWord(150),
+            Description = "GRPC" + RandomData.GenerateWord(250)
+        };
+        AddBusinessDepartmentResponse addBusinessDepartmentResponse = businessDepartmentServiceClient.AddBusinessDepartment(new AddBusinessDepartmentRequest() { BusinessDepartmentModel = businessDepartmentModel });
+
+        Assert.IsTrue(addBusinessDepartmentResponse.Success, addBusinessDepartmentResponse.Message);
+    }
+
+    [TestMethod]
+    public void EditBusinessDepartmentTest()
+    {
+        // The port number must match the port of the gRPC server.
+        using var channel = GrpcChannel.ForAddress("https://localhost:7108");
+        var businessDepartmentServiceClient = new BusinessDepartmentService.BusinessDepartmentServiceClient(channel);
+        ListBusinessDepartmentResponse listBusinessDepartmentResponse = businessDepartmentServiceClient.ListBusinessDepartment(new ListBusinessDepartmentRequest());
+        var businessDepartmentModelEdit = listBusinessDepartmentResponse.ResultItem.LastOrDefault();
+        if(businessDepartmentModelEdit is null)
+        {
+            businessDepartmentModelEdit = new BusinessDepartmentModel();
+        }
+        businessDepartmentModelEdit.Name = "GRPC 2" + RandomData.GenerateWord(150);
+        businessDepartmentModelEdit.Description = "GRPC 2" + RandomData.GenerateWord(250);
+        var editaBusinessDepartmentResponse = businessDepartmentServiceClient.EditBusinessDepartment(new EditBusinessDepartmentRequest(){ BusinessDepartmentModel = businessDepartmentModelEdit});
+
+        Assert.IsTrue(editaBusinessDepartmentResponse.Success, editaBusinessDepartmentResponse.Message);
+    }
+}
--- ContactServiceGRPC/Services/BusinessDepartmentService.cs
+++ ContactServiceGRPC/Services/BusinessDepartmentService.cs
@@ -15,8 +15,15 @@ public class BusinessDepartmentService : ContactServiceGRPC.BusinessDepartmentSe
 
     public override Task<ListBusinessDepartmentResponse> ListBusinessDepartment(ListBusinessDepartmentRequest request, ServerCallContext context)
     {
-        ListBusinessDepartmentResponse response = new ListBusinessDepartmentResponse();
         IResponseInformation<BusinessDepartment[]> responseInformation = BLBusinessDepartment.ListBusinessDepartment();
+
+        ListBusinessDepartmentResponse response = new ListBusinessDepartmentResponse()
+        {
+            Success = responseInformation.Success,
+            Message = responseInformation.Message,
+            Failure = responseInformation.Failure
+        };
+        
         if (responseInformation.Success && responseInformation.ResultItem is not null)
         {
             foreach (var department in responseInformation.ResultItem)
@@ -24,12 +31,51 @@ public class BusinessDepartmentService : ContactServiceGRPC.BusinessDepartmentSe
                 response.ResultItem.Add(new BusinessDepartmentModel()
                 {
                     BusinessDepartmentId = department.BusinessDepartmentId,
-                    Name = department.Name,
-                    Description = department.Description
+                    Name = department.Name ?? string.Empty,
+                    Description = department.Description ?? string.Empty
                 });
 
             }
         }
+
+
         return Task.FromResult(response);
+    }
+
+    public override Task<AddBusinessDepartmentResponse> AddBusinessDepartment(AddBusinessDepartmentRequest request, ServerCallContext context)
+    {
+        BusinessDepartment businessDepartment = new BusinessDepartment()
+        {
+            Name = request.BusinessDepartmentModel.Name,
+            Description = request.BusinessDepartmentModel.Description
+        };
+        IResponseInformation responseInformation = BLBusinessDepartment.AddBusinessDepartment(businessDepartment);
+
+        AddBusinessDepartmentResponse addBusinessDepartmentResponse = new AddBusinessDepartmentResponse()
+        {
+            Success = responseInformation.Success,
+            Message = responseInformation.Message,
+            Failure = responseInformation.Failure
+        };
+        return Task.FromResult(addBusinessDepartmentResponse);
+    }
+
+    public override Task<EditaBusinessDepartmentResponse> EditBusinessDepartment(EditBusinessDepartmentRequest request, ServerCallContext context)
+    {
+        BusinessDepartment businessDepartment = new BusinessDepartment()
+        {
+            BusinessDepartmentId = request.BusinessDepartmentModel.BusinessDepartmentId,
+            Name = request.BusinessDepartmentModel.Name,
+            Description = request.BusinessDepartmentModel.Description
+        };
+        IResponseInformation<BusinessDepartment> responseInformation = BLBusinessDepartment.EditBusinessDepartment(businessDepartment);
+
+        EditaBusinessDepartmentResponse editaBusinessDepartmentResponse = new EditaBusinessDepartmentResponse()
+        {
+            Success = responseInformation.Success,
+            Message = responseInformation.Message,
+            Failure = responseInformation.Failure
+        };
+        return Task.FromResult(editaBusinessDepartmentResponse);
     }
 }
